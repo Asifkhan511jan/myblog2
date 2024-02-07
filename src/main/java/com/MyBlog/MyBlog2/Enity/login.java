6917997476fbfaf4749e859a9c76164a2bed1d0b
Asifkhan511jan <Asifkhan511jan@gmail.com>
2024-02-07 15:21:29 +0530

    127:Create  Save Delete Update Post Feature

---
+++ src/main/java/com/MyBlog/MyBlog2/Enity/login.java
@@ -0,0 +1,11 @@
+package com.MyBlog.MyBlog2.Enity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class login {
+
+    private long id;
+    private String email;
+    private  String password;
+}
